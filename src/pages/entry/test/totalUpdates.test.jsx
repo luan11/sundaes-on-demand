3994bfa13c76f4e2f63133d944dea12614eea972
Luan Novais <luaannovais@live.com>
2022-01-12 20:07:51 -0300

    Test: Add provider

--- src/pages/entry/test/totalUpdates.test.jsx
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -1,15 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils';
 import userEvent from '@testing-library/user-event';
 
 import { OPTION_TYPE_SCOOPS } from '../../../constants/entry';
 import Options from '../Options';
-import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
 
 describe(`totalUpdates`, () => {
   it(`Should update scoop subtotal when scoops change`, async () => {
-    render(<Options optionType={OPTION_TYPE_SCOOPS} />, {
-      wrapper: OrderDetailsProvider,
-    });
+    render(<Options optionType={OPTION_TYPE_SCOOPS} />);
 
     const scoopSubtotal = await screen.findByText(`Scoops total: $`, {
       exact: false,
